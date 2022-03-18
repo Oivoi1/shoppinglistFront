d1ed98bab7167431dda92ff4d7038ca5f422e0e5
Veeti Oivo <MY_NAME@example.com>
2022-03-18 12:13:35 +0200

    first commit

---
+++ shoppinglist/src/App.js
@@ -0,0 +1,77 @@
+import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios"
+
+const URL = "http://localhost/shoppinglist/";
+
+function App() {
+  const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
+  const [amount, setAmount] = useState("")
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setItems(response.data);
+      }).catch(error =>{
+        alert(error.response ? error.response.data.error: error);
+      });
+  }, [])
+
+  function save(e){
+    e.preventDefault();
+    const json = JSON.stringify({description:item, amount:amount});
+    axios.post(URL + "add.php", json, {
+      headers: {
+        "Content-Type" : "application/json"
+      }
+    })
+    .then((response) => {
+      setItems(items => [...items,response.data]);
+      setItem("");
+      setAmount("");
+    }).catch(error =>{
+      alert(error.response ? error.response.data.error: error);
+    });
+  }
+
+  function remove(id){
+    const json = JSON.stringify({id:id});
+    axios.post(URL + "delete.php", json, {
+      headers: {
+        "Content-Type" : "application/json"
+      }
+    })
+    .then((response) => {
+      const newListWithoutRemoved = items.filter((item) => item.id !==id);
+      setItems(newListWithoutRemoved)
+    }).catch(error =>{
+      alert(error.response ? error.response.data.error: error);
+    });
+  }
+
+
+  return (
+    <div className="container">
+      <form onSubmit={save}>
+        <label>New item</label>
+        <input value={item} placeholder='type description' onChange={e => setItem(e.target.value)}></input>
+        <label>New item</label>
+        <input value={amount} placeholder='type amount' onChange={e => setAmount(e.target.value)}></input>
+        <button>Add</button>
+      </form>
+      <ol>
+        {items?.map(item=>(
+          <li key={item.id}>
+          {item.description}&nbsp;{item.amount}&nbsp;
+            <a href="#" className='delete' onClick={() => remove(item.id)}>
+              Delete
+            </a>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
